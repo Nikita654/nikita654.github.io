8f3f7c0fc87898a34fefba634ed9a80aabd38894
unknown <Nikita>
2019-03-02 00:19:52 +0300

    Add new animations

--- Creativemain/main.js
+++ Creativemain/main.js
@@ -1,2 +1,25 @@
 function scrolle()
 {window.scrollBy(0,700)};
+
+var doAnimations = function() {
+    let offset =  window.screen.availHeight + window.pageYOffset,
+        animatables = document.getElementsByClassName('animatable');
+    if (animatables.length == 0) {
+      window.removeEventListener('scroll', doAnimations);
+    }
+   
+    for(let i=0;i<animatables.length;i++){
+      let animatable = animatables[i],counter;
+      if (window.screen.availHeight<animatable.clientHeight) {counter=800} else counter=40;
+      if ((animatable.getBoundingClientRect().top + animatable.clientHeight  + window.pageYOffset-counter) < offset) {
+      animatable.classList.remove('animatable');
+      animatable.classList.add('animated');
+      };
+    };};
+    doAnimations();
+    doAnimations();
+    doAnimations();
+    window.addEventListener('load', doAnimations,false);
+    window.addEventListener('scroll', doAnimations,false);
+   
+    doAnimations();
