f154fd59cead317fba80c26969a2abb0477cd84e
unknown <Nikita>
2019-07-11 14:26:18 +0300

    add more requests

--- woop/reqs.js
+++ woop/reqs.js
@@ -34,10 +34,11 @@ function liders() {
         rek2= JSON.parse(xhr.response)
         let names=document.getElementsByClassName('liders-names');
             let money=document.getElementsByClassName('liders-payments');
-            
+            let avatar=document.getElementsByClassName('liders-get-avatar');
       for (i = 0; i < 4; i++) {
         names[i].innerHTML=rek2.leaders.by_sum[i].username;
         money[i].innerHTML=rek2.leaders.by_sum[i].total_bets.sum;
+        avatar[i].src=rek2.leaders.by_sum[i].avatar;
       }
        
         
@@ -81,13 +82,15 @@ function payments() {
       xhr.onreadystatechange = function() {
           if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { 
             rek2= JSON.parse(xhr.response)
-  
+            let avatar=document.getElementsByClassName('oplata-get-avatar');
             let names=document.getElementsByClassName('oplata-get-name');
             let money=document.getElementsByClassName('oplata-get-money');
             
       for (i = 0; i < 4; i++) {
-        names[i].innerHTML=rek2.withdraws[i].username
-        money[i].innerHTML=rek2.withdraws[i].amount
+        names[i].innerHTML=rek2.withdraws[i].username;
+        money[i].innerHTML=rek2.withdraws[i].amount;
+        avatar[i].src=rek2.withdraws[i].avatar;
+         
       }
       
 
